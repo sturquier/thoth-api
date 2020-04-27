56d90002fb0a31abd2b220989897ebf2d3552b27
sturquier <turquiersimon@hotmail.fr>
2020-04-27 22:19:45 +0200

    Setup Nelmio API doc

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
