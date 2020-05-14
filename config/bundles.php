58c85fe3b80c63135d0944a669be30d76786bfcf
sturquier <turquiersimon@hotmail.fr>
2020-05-14 23:59:54 +0200

    Fix CORS

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
